448834c931a2a140cdc461ba063d04e004169e02
r0b3rTo <roberto71189@gmail.com>
2013-02-22 14:27:02 -0430

    Actualización pokemon.hs

--- pokemon.hs
+++ pokemon.hs
@@ -9,7 +9,10 @@ module Pokemon
   , daño
   )
 where
-  
+
+import Data.List
+import Data.Char
+import Data.Array
 
 data Tipo
   = Bug
@@ -42,11 +45,63 @@ data Especie
     , ataqueEspecial :: Int
     , defensaEspecial :: Int
     , velocidad :: Int
-    , preEvolucion :: [Int]
+    , preEvolucion :: Int
     , evolucion :: String
     }
   deriving (Read, Show)
+
+
+crearEspecie :: [String] -> Especie
+crearEspecie datos
+   | null $ datos!!3 = Especie {numero = read $ datos!!0::Int, 
+                              nombreEspecie = datos!!1, 
+                              tipoEspecie = read $ datos!!2::Either Tipo (Tipo,Tipo), 
+                              hpEspecie = read $ datos!!4::Int,
+                              ataque = read $ datos!!5::Int, 
+                              defensa = read $ datos!!6::Int,
+                              ataqueEspecial = read $ datos!!7::Int, 
+                              defensaEspecial = read $ datos!!8::Int, 
+                              velocidad = read $ datos!!9::Int,
+                              preEvolucion = read $ datos!!10::Int,
+                              evolucion = datos!!11}
+   | otherwise = Especie {numero = read $ datos!!0::Int, 
+                        nombreEspecie = datos!!1, 
+                        tipoEspecie = tuplaTipo, 
+                        hpEspecie = read $ datos!!4::Int,
+                        ataque = read $ datos!!5::Int, 
+                        defensa = read $ datos!!6::Int,
+                        ataqueEspecial = read $ datos!!7::Int, 
+                        defensaEspecial = read $ datos!!8::Int, 
+                        velocidad = read $ datos!!9::Int,
+                        preEvolucion = read $ datos!!10::Int,
+                        evolucion = datos!!11}
+      where
+         tuplaTipo = read $ "("++datos!!2++","++datos!!3++")"::Either Tipo (Tipo,Tipo)
+         
+buscarEspecie :: Array Int Especie -> Int -> Especie
+buscarEspecie pokedex numeroEspecie = pokedex!numeroEspecie
   
+imprimirEspecie :: Especie -> IO()
+imprimirEspecie especie = do
+   putStr "Número: "
+   print $ numero especie
+   putStr "Nombre: "
+   print $ nombreEspecie especie
+   putStr "Tipo: "
+   print $ tipoEspecie especie
+   putStr "HP: "
+   print $ hpEspecie especie
+   putStr "Ataque: "
+   print $ ataque especie
+   putStr "Defensa: "
+   print $ defensa especie
+   putStr "Ataque Especial: "
+   print $ ataqueEspecial especie
+   putStr "Defensa Especial: "
+   print $ defensaEspecial especie
+   putStr "Velocidad: "
+   print $ velocidad especie
+   
   
 data Ataque
   = Ataque
@@ -58,6 +113,24 @@ data Ataque
     }
   deriving (Read, Show)
   
+crearAtaque :: [String] -> Ataque
+crearAtaque datos = Ataque {nombreAtaque = datos!!0,
+                        tipoAtaque = read $ datos!!1::Tipo,
+                        forma = read $ datos!!2::Bool,
+                        puntoPoder = read $ datos!!3::Int,
+                        poder = read $ datos!!4::Int}
+
+
+buscarAtaque :: [Ataque] -> String -> Int -> Int -> Maybe Ataque
+buscarAtaque ataqueDex nombreAtaque cotaMinima cotaMaxima
+   | cotaMaxima < cotaMinima = Nothing
+   | nombreAtaque $ ataqueDex!!mid > nombreAtaque = buscarAtaque ataqueDex nombreAtaque cotaMinima (mid-1)
+   | nombreAtaque $ ataqueDex!!mid < nombreAtaque = buscarAtaque ataqueDex nombreAtaque (mid+1) cotaMaxima
+   | otherwise = Just ataqueDex!!mid 
+      where
+         mid = cotaMinima + ((cotaMaxima – cotaMinima) `div` 2)
+
+   
 data Monstruo
   = Monstruo
     { especie :: Especie
@@ -76,23 +149,42 @@ data Monstruo
     }
   deriving (Read, Show)
   
-maxHp ::  Monstruo -> Int
-maxHp monstruo = ((31 + 2 * hpEspecie (especie monstruo) + 255 `quot` 4 + 100) * nivel monstruo `div` 100 ) + 10  
+-- crearMonstruo :: Array Int Especie -> [Ataque] -> [String] -> Monstruo
+-- crearMonstruo pokedex ataqueDex datos = 
+--    Monstruo {especie = especieMonstruo,
+--       sobreNombre = datos!!1,
+--       nivel = nivelMonstruo,
+--       hp = maxHp especieMonstruo nivelMonstruo,
+--       ataqueM = estadisticaAtaque especieMonstruo nivelMonstruo,
+--       defensaM = estadisticaDefensa especieMonstruo nivelMonstruo,
+--       ataqueEM = estadisticaAtaqueE especieMonstruo nivelMonstruo,
+--       defensaEM = estadisticaDefensaE especieMonstruo nivelMonstruo,
+--       velocidadM = estadisticaVelocidad especieMonstruo nivelMonstruo,
+--       ataque1 = buscarAtaque ataqueDex datos!!3 0 length ataqueDex,
+--       ataque2 =,
+--       ataque3 =,
+--       ataque4 =}
+--    where
+--       especieMonstruo = buscarEspecie pokedex read $ datos!!0::Int
+--       nivelMonstruo = read $ datos!!2::Int
+  
+maxHp ::  Especie -> Int -> Int
+maxHp especie nivel = ((31 + 2 * hpEspecie especie + 255 `quot` 4 + 100) * nivel `div` 100 ) + 10  
 
-estadisticaAtaque :: Monstruo -> Int
-estadisticaAtaque monstruo = ((31 + 2 * ataque (especie monstruo) + 63) * nivel monstruo `div` 100 ) + 5 
+estadisticaAtaque :: Especie -> Int -> Int
+estadisticaAtaque especie nivel = ((31 + 2 * ataque especie + 63) * nivel `div` 100 ) + 5 
 
-estadisticaDefensa :: Monstruo -> Int
-estadisticaDefensa monstruo = ((31 + 2 * defensa (especie monstruo) + 63) * nivel monstruo `div` 100 ) + 5 
+estadisticaDefensa :: Especie -> Int -> Int
+estadisticaDefensa especie nivel = ((31 + 2 * defensa especie + 63) * nivel `div` 100 ) + 5 
 
-estadisticaAtaqueE :: Monstruo -> Int
-estadisticaAtaqueE monstruo = ((31 + 2 * ataqueEspecial (especie monstruo) + 63) * nivel monstruo `div` 100 ) + 5 
+estadisticaAtaqueE :: Especie -> Int -> Int
+estadisticaAtaqueE especie nivel = ((31 + 2 * ataqueEspecial especie + 63) * nivel `div` 100 ) + 5 
 
-estadisticaDefensaE :: Monstruo -> Int
-estadisticaDefensaE monstruo = ((31 + 2 * defensaEspecial (especie monstruo) + 63) * nivel monstruo `div` 100 ) + 5 
+estadisticaDefensaE :: Especie -> Int -> Int
+estadisticaDefensaE especie nivel = ((31 + 2 * defensaEspecial especie + 63) * nivel `div` 100 ) + 5 
 
-estadisticaVelocidad :: Monstruo -> Int
-estadisticaVelocidad monstruo = ((31 + 2 * velocidad (especie monstruo) + 63) * nivel monstruo `div` 100 ) + 5 
+estadisticaVelocidad :: Especie -> Int -> Int
+estadisticaVelocidad especie nivel = ((31 + 2 * velocidad especie + 63) * nivel `div` 100 ) + 5 
 
 -- Determina, para un tipo de ataque, cuales tipos son super efectivos,
 -- cuales tipos son resistentes y cuales son inmunes.
@@ -123,18 +215,18 @@ relacionAtaqueTipo x
 
 
 daño :: Monstruo -> Monstruo -> Ataque ->  Double
-daño atacante defensor ataque = golpeFinal 
+daño atacante defensor ataque = golpeFinal
   where
    golpeFinal = multiplicador (fromIntegral golpeTotal)
    multiplicador golpe = case tipoEspecie ( especie atacante ) of
       Left tipo          ->  if tipoAtaque ataque == tipo
                              then   (golpe) * 1.5
                              else golpe
-      Right(tipo1,tipo2) ->  if  tipoAtaque ataque == tipo1 ||tipoAtaque ataque == tipo2 
+      Right(tipo1,tipo2) ->  if  tipoAtaque ataque == tipo1 || tipoAtaque ataque == tipo2 
                              then   (golpe) * 1.5
                              else golpe
    golpeTotal =  modificadorAtacante golpe
-   golpe = ((fuerzaAtacante * poderAtaque * lucha ) `div` 50 )+ 2
+   golpe = ((fuerzaAtacante * poderAtaque * lucha ) `div` 50 ) + 2
    fuerzaAtacante = (2 * nivel atacante `div` 5 ) + 2
    poderAtaque =  poder ataque
    lucha = case forma ataque of
@@ -150,18 +242,28 @@ daño atacante defensor ataque = golpeFinal
                         if terceraLista tipo 
                         then 0 
                         else golpe
-      Right (tipo1,tipo2) -> if primeraLista tipo1 || primeraLista tipo2
-                             then golpe * 2
+      Right (tipo1,tipo2) -> if primeraLista tipo1 && primeraLista tipo2
+                             then golpe * 4
                              else
-                              if segundaLista tipo1 || segundaLista tipo2
-                              then golpe `div` 2
-                              else 
-                               if terceraLista tipo1 || terceraLista tipo2
-                               then 0
-                               else golpe 
+                              if (primeraLista tipo1 && not(primeraLista tipo2)) || 
+                                 (not(primeraLista tipo1) && primeraLista tipo2)
+                              then golpe * 2
+                              else
+                                 if segundaLista tipo1 && segundaLista tipo2
+                                 then golpe `div` 4 
+                                 else
+                                    if (segundaLista tipo1 && not(segundaLista tipo2)) ||
+                                       (not(segundaLista tipo1) && segundaLista tipo2)
+                                    then golpe `div` 2
+                                    else 
+                                       if terceraLista tipo1 || terceraLista tipo2
+                                       then 0
+                                       else golpe 
       
       
    primeraLista tipo = elem tipo ((\(a,b,c)-> a)  (relacionAtaqueTipo  (tipoAtaque ataque)))
    segundaLista tipo = elem tipo ((\(a,b,c)-> b)  (relacionAtaqueTipo  (tipoAtaque ataque)))
    terceraLista tipo = elem tipo ((\(a,b,c)-> c)  (relacionAtaqueTipo  (tipoAtaque ataque)))
-    
+   
+crearPokedex :: [Especie] -> Array Int Especie
+crearPokedex listaEspecies = array (1,251) [(i, x) | i <- [1..251], x <- [(!!) listaEspecies (i-1)]]
